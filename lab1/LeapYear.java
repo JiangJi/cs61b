a75210d4dbb490554be6ceebf33dc468cebe8aa9
YJ <yangjianzjucs@gmail.com>
2015-06-23 15:57:47 +0800

    Completed LeapYear.java

---
+++ lab1/LeapYear.java
@@ -0,0 +1,10 @@
+// Adapted from Sierra and Bates, Head First Java
+
+// You need not use all of the lines below, and you may use duplicates.
+public class LeapYear
+{
+  public static void main(String[] args)
+  {
+    System.out.println("hello world");
+  }
+}
